ee693b46fe276447611b16da1765c89ffc17b066
gzoritchak <g.zoritchak@gmail.com>
2014-11-10 23:27:09 +0100

    Simplify.

---
+++ fluent-helloworld-master/build-and-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+start_time=`date +%s`
+
+docker build -t hello-mvn .
+
+docker rm -f hello-compiled
+docker run \
+    --name hello-compiled \
+    -v ~/.m2:/root/.m2 \
+    hello-mvn install -DskipTests
+
+docker rm -f hello-mvn
+docker run -d \
+    --name hello-mvn \
+    -v ~/.m2:/root/.m2 \
+    --volumes-from hello-compiled \
+    -p 8080:8080 \
+    hello-mvn exec:java -DskipTests
+
+echo New container started in $(expr `date +%s` - $start_time) s
--- images/maven-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker run  \
-        -v $PWD/.m2:/root/.m2 \
-        --name maven-cache \
-        busybox
